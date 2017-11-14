6856c2a6408e5abf3f4b2f08fc7290a0546d962f
Catch-XXII <cgultekink@gmail.com>
2017-11-14 17:01:25 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.close = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.previous = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.create = new System.Windows.Forms.Button();
+            this.search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(14, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(534, 260);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(552, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // close
+            // 
+            this.close.Image = global::MusicPlayer.Properties.Resources.Cancel;
+            this.close.Location = new System.Drawing.Point(500, 12);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(48, 48);
+            this.close.TabIndex = 9;
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click_1);
+            // 
+            // next
+            // 
+            this.next.Image = global::MusicPlayer.Properties.Resources.skip_next;
+            this.next.Location = new System.Drawing.Point(446, 12);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(48, 48);
+            this.next.TabIndex = 8;
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click_1);
+            // 
+            // stop
+            // 
+            this.stop.Image = global::MusicPlayer.Properties.Resources.Stop;
+            this.stop.Location = new System.Drawing.Point(392, 12);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(48, 48);
+            this.stop.TabIndex = 7;
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click_1);
+            // 
+            // play
+            // 
+            this.play.Image = global::MusicPlayer.Properties.Resources.Play;
+            this.play.Location = new System.Drawing.Point(339, 12);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(48, 48);
+            this.play.TabIndex = 6;
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click_1);
+            // 
+            // pause
+            // 
+            this.pause.Image = global::MusicPlayer.Properties.Resources.Pause;
+            this.pause.Location = new System.Drawing.Point(282, 12);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(48, 48);
+            this.pause.TabIndex = 5;
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click_1);
+            // 
+            // previous
+            // 
+            this.previous.Image = global::MusicPlayer.Properties.Resources.skip_previous;
+            this.previous.Location = new System.Drawing.Point(228, 12);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(48, 48);
+            this.previous.TabIndex = 4;
+            this.previous.UseVisualStyleBackColor = true;
+            this.previous.Click += new System.EventHandler(this.previous_Click_1);
+            // 
+            // delete
+            // 
+            this.delete.Image = global::MusicPlayer.Properties.Resources.Delete;
+            this.delete.Location = new System.Drawing.Point(174, 12);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(48, 48);
+            this.delete.TabIndex = 3;
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click_1);
+            // 
+            // show
+            // 
+            this.show.Image = global::MusicPlayer.Properties.Resources.Show;
+            this.show.Location = new System.Drawing.Point(120, 12);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(48, 48);
+            this.show.TabIndex = 2;
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click_1);
+            // 
+            // create
+            // 
+            this.create.Image = global::MusicPlayer.Properties.Resources.Create;
+            this.create.Location = new System.Drawing.Point(68, 12);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(48, 48);
+            this.create.TabIndex = 1;
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click_1);
+            // 
+            // search
+            // 
+            this.search.Image = global::MusicPlayer.Properties.Resources.Search;
+            this.search.Location = new System.Drawing.Point(14, 12);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(48, 48);
+            this.search.TabIndex = 0;
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Show";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Create";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Search";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Delete";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Previous";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(282, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Pause";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(346, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 17);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Play";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(398, 63);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 17);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Stop";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(452, 63);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Next";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(503, 63);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 17);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Close";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 359);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 17);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Status:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(64, 359);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 17);
+            this.label12.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 380);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.previous);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.search);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button previous;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        private static string path = @"C:\MyPlayList";
+        static int currentsong = 0;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            play.Enabled = false;
+            pictureBox1.Image = Image.FromFile("Default.jpg");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentsong = listBox1.SelectedIndex;
+            string pictureName = listBox1.SelectedItem.ToString().Split('-')[0].Trim() + ".jpg";
+            string path1 = path + "\\" + listBox1.SelectedItem;
+            wplayer.URL = path1;
+            wplayer.controls.play();
+            label12.Text = "Playing " + listBox1.SelectedItem;
+            if (File.Exists(pictureName))
+                pictureBox1.Image = Image.FromFile(pictureName);
+            else
+            {
+                pictureBox1.Image = Image.FromFile("Default.jpg");
+            }
+        }
+
+        #region EnvironmentButtons
+
+        private void search_Click_1(object sender, EventArgs e)
+        {
+            String[] allmp3 = System.IO.Directory.GetFiles(path, "*.mp3", System.IO.SearchOption.AllDirectories);
+            label12.Text = Convert.ToString(allmp3.Length) + " songs are in your directory";
+        }
+
+        private void create_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    MessageBox.Show(path + " That path already exists.");
+                    return;
+                }
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                MessageBox.Show(path + " The directory was created successfully.");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("The process failed");
+                throw;
+            }
+        }
+
+        private void show_Click_1(object sender, EventArgs e)
+        {
+            play.Enabled = true;
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("There is no directory.\nPlease create one");
+                return;
+            }
+
+            listBox1.Items.Clear();
+            string[] lines = Directory.GetFiles(path);
+            foreach (string line in lines)
+            {
+                string[] divided = line.Split('\u005c');
+                string l1 = divided[0];
+                string l2 = divided[1];
+                string l3 = divided[2];
+                listBox1.Items.Add(l3);
+            }
+        }
+
+        private void delete_Click_1(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("No directory found");
+                return;
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you Sure? \nIf you click 'Yes', All files will be deleted", "Delete Confirmation", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes && Directory.Exists(path))
+                {
+                    MessageBox.Show("Your library was deleted");
+                    Directory.Delete(path, true);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void close_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region ControlButtons
+
+        private void play_Click_1(object sender, EventArgs e)
+        {
+            label12.Text = "Playing " + listBox1.SelectedItem;
+            wplayer.controls.stop();
+            wplayer.URL = path + "\\" + listBox1.SelectedItem.ToString();
+            wplayer.controls.play();
+        }
+
+        private void pause_Click_1(object sender, EventArgs e)
+        {
+            label12.Text = "Paused...";
+            wplayer.controls.pause();
+        }
+
+        private void stop_Click_1(object sender, EventArgs e)
+        {
+            label12.Text = "Stopped...";
+            wplayer.controls.stop();
+        }
+
+        private void next_Click_1(object sender, EventArgs e)
+        {
+            if (currentsong + 1 < listBox1.Items.Count)
+            {
+                currentsong = currentsong + 1;
+                listBox1.SelectedIndex = currentsong;
+                label12.Text = "Playing " + listBox1.Items[currentsong].ToString();
+                wplayer.controls.stop();
+                wplayer.URL = path + "\\" + listBox1.Items[currentsong].ToString();
+                wplayer.controls.play();
+
+            }
+        }
+
+        private void previous_Click_1(object sender, EventArgs e)
+        {
+
+            if (currentsong - 1 < listBox1.Items.Count)
+            {
+                currentsong = currentsong - 1;
+                listBox1.SelectedIndex = currentsong;
+                label12.Text = "Playing " + listBox1.Items[currentsong].ToString();
+                wplayer.controls.stop();
+                wplayer.URL = path + "\\" + listBox1.Items[currentsong].ToString();
+                wplayer.controls.play();
+            }
+        }
+
+        #endregion
+    }
+}
